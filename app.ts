31f97ef63ff6e2bf5a96923a578041c39af233c2
Andrew Khazanovich <akhazanovich+GIT@gmail.com>
2017-01-16 17:50:39 -0500

    Completed article component

--- app.ts
+++ app.ts
@@ -15,6 +15,24 @@ class Article {
     this.link = link;
     this.votes = votes || 0;
   }
+
+  voteUp(): void {
+    this.votes += 1;
+  }
+
+  voteDown(): void {
+    this.votes -= 1;
+  }
+
+  domain(): string {
+    try {
+        const link: string = this.link.split('//')[1];
+        return link.split('/')[0];
+    } catch (err){
+      return null;
+    }
+  }
+
 }
 
 @Component({
@@ -62,14 +80,15 @@ class ArticleComponent {
   }
 
   voteUp(): boolean {
-    this.article.votes += 1;
+    this.article.voteUp();
     return false;
   }
 
   voteDown(): boolean {
-    this.article.votes -= 1;
+    this.article.voteDown();
     return false;
   }
+  
 }
 
 @Component({
